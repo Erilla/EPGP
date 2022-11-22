67e2e25923afb244e954a652f27abc80fce9a5f0
Ryan Wong <me@ryanwong.uk>
2022-11-22 22:51:35 +0000

    Added Auth0 authentication

--- EPGP.API/Program.cs
+++ EPGP.API/Program.cs
@@ -1,4 +1,9 @@
 using Auth0.AspNetCore.Authentication;
+using EPGP.API.Filters;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,17 +13,59 @@ var config = new ConfigurationBuilder()
                  .Build();
 
 
-// Add services to the container.
-builder.Services.AddAuth0WebAppAuthentication(options => {
-    options.Domain = config["Auth0:Domain"];
-    options.ClientId = config["Auth0:ClientId"];
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = config["Authentication:Domain"];
+    options.Audience = config["Authentication:Audience"];
 });
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+            new OpenApiInfo
+            {
+                Title = "API",
+                Version = "v1",
+                Description = "A REST API",
+                TermsOfService = new Uri("https://lmgtfy.com/?q=i+like+pie")
+            });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow
+            {
+                Scopes = new Dictionary<string, string>
+                {
+                    { "all", "All" }
+                },
+                AuthorizationUrl = new Uri(config["Authentication:Domain"] + "authorize?audience=" + config["Authentication:Audience"])
+            }
+        }
+    });
+
+    c.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 
 var app = builder.Build();
 
@@ -26,7 +73,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+        c.OAuthClientId(config["Authentication:ClientId"]);
+    });
 }
 
 app.UseHttpsRedirection();
